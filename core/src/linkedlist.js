a3ec456ebf4e55e72f39fe13e84803e5ba249b0c
tyshonka <dasha.yarova.0@gmail.com>
2026-04-07 12:00:00 +0300

    lab 4

--- core/src/linkedlist.js
+++ core/src/linkedlist.js
@@ -1,6 +1,19 @@
-export class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
+export function rearrangeList(head) {
+    let negHead = null, negTail = null;
+    let posHead = null, posTail = null;
+    let current = head;
+
+    while (current) {
+        let next = current.next;
+        current.next = null;
+        if (current.value < 0) {
+            if (!negHead) negHead = negTail = current;
+            else { negTail.next = current; negTail = current; }
+        } else {
+            if (!posHead) posHead = posTail = current;
+            else { posTail.next = current; posTail = current; }
+        }
+        current = next;
     }
+    return negHead || posHead; 
 }
