c00452a24f8ca8e80f8d6e39ec7d46413b10cf72
tyshonka <dasha.yarova.0@gmail.com>
2026-05-03 11:00:00 +0300

    lab 6: adds

--- core/src/streamProssecor.js
+++ core/src/streamProssecor.js
@@ -6,3 +6,26 @@ export async function* createDataStream(count) {
         yield { id: i, payload: `Task data ${i}` };
     }
 }
+
+import { EventEmitter } from 'events';
+
+export async function consumeStream(stream, processor) {
+    for await (const chunk of stream) {
+        processor(chunk);
+    }
+}
+
+export class EventStream extends EventEmitter {
+    process(dataArray) {
+        for (const item of dataArray) {
+            try {
+                if (!item) throw new Error("Null data received");
+                this.emit('data', item);
+            } catch (err) {
+                this.emit('error', err);
+                throw err; 
+            }
+        }
+        this.emit('end');
+    }
+}
