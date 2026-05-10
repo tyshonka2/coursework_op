f8194ca8f25fbb4ebca92d9841a0636cd3bccd93
tyshonka <dasha.yarova.0@gmail.com>
2026-05-10 10:00:00 +0300

    lab 7

---
+++ core/src/reactiveSystem.js
@@ -0,0 +1,22 @@
+import { EventEmitter } from 'events';
+
+export class ReactiveEntity extends EventEmitter {
+    emit(event, ...args) {
+        const listeners = this.listeners(event);
+        
+        if (event === 'error' && listeners.length === 0) {
+            throw args[0] || new Error('Unhandled reactive error');
+        }
+
+        let hasListeners = false;
+        for (const listener of listeners) {
+            hasListeners = true;
+            try {
+                listener.apply(this, args);
+            } catch (err) {
+                this.emit('error', err);
+            }
+        }
+        return hasListeners;
+    }
+}
