a236803ac7934db1c723756c210b47fbce4c3216
tyshonka <dasha.yarova.0@gmail.com>
2026-05-15 14:00:00 +0300

    lab 8

--- core/src/httpSystem.js
+++ core/src/httpSystem.js
@@ -4,3 +4,38 @@ export class BaseHttpClient {
         return response;
     }
 }
+
+export class JwtAuthStrategy {
+    constructor(getToken, refreshToken) {
+        this.getToken = getToken; 
+        this.refreshToken = refreshToken; 
+    }
+
+    inject(options) {
+        const headers = new Headers(options.headers || {});
+        headers.set('Authorization', `Bearer ${this.getToken()}`);
+        return { ...options, headers };
+    }
+
+    async refresh() {
+        if (this.refreshToken) {
+            await this.refreshToken();
+        }
+    }
+}
+
+// Стратегія 2: API Key
+export class ApiKeyStrategy {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    inject(options) {
+        const headers = new Headers(options.headers || {});
+        headers.set('X-API-Key', this.apiKey);
+        return { ...options, headers };
+    }
+
+    async refresh() { 
+    }
+}
